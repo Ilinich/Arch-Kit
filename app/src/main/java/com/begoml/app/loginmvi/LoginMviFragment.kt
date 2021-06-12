552b9fcf2d0a940aeeaa001377e99b44579c4a46
Roman Zhurid <Roman.Zhurid@solbeg.com>
2021-06-12 19:23:37 +0300

    refactoring package

--- app/src/main/java/com/begoml/app/loginmvi/LoginMviFragment.kt
+++
@@ -1,5 +0,0 @@
-package com.begoml.app.loginmvi
-
-import androidx.fragment.app.Fragment
-
-class LoginMviFragment : Fragment()
--- app/src/main/java/com/begoml/app/profile/ProfileFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.begoml.app.profile
-
-import androidx.fragment.app.Fragment
-import com.begoml.app.R
-
-class ProfileFragment: Fragment(R.layout.fragment_profile)
